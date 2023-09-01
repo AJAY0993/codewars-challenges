6a4a6f34e2ed2f8351118533f0464826c39c2a69
AJAY0993 <128742355+AJAY0993@users.noreply.github.com>
2023-09-01 23:28:06 +0530

    Create Name Shuffler.js

---
+++ 8kyu/Name Shuffler.js
@@ -0,0 +1,13 @@
+//my code
+
+function nameShuffler(str){
+  //Shuffle It
+  let arr = str.split(' ')
+   let fName = arr[0]
+   let sName = arr[1]
+   
+   arr[0] = sName
+   arr[1] = fName
+  
+  return arr.join(' ')
+}

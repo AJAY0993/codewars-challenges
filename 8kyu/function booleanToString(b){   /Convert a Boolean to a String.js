e75ac1236eed7def9aff38d06005ad252a90c00f
AJAY0993 <ajaymishra5277919886@gmail.com>
2023-10-04 23:21:40 +0530

    Delete 8kyu/function booleanToString(b){    directory

--- 8kyu/function booleanToString(b){   /Convert a Boolean to a String.js
+++
@@ -1,9 +0,0 @@
-// DESCRIPTION:
-// Implement a function which convert the given boolean value into its string representation.
-
-// Note: Only valid inputs will be given.
-  
-function booleanToString(b){
-  //your code here
-  return String(b)
-}
